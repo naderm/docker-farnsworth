4d7577d30af00d1f9216c019ec1816dc379e3f29
Nader Morshed <morshed.nader@gmail.com>
2015-02-04 23:21:16 -0500

    Use vanilla postgres image

--- settings/local_settings.py
+++ settings/local_settings.py
@@ -7,23 +7,23 @@ DEBUG = False
 TEMPLATE_DEBUG = None
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'docker',
-        'USER': 'postgres',
-        'PASSWORD': os.environ.get('POSTGRESQL_PASS', ''),
-        'HOST': os.environ.get('DB_1_PORT_5432_TCP_ADDR'),
-        'PORT': os.environ.get('DB_1_PORT_5432_TCP_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": os.environ.get("DB_1_PORT_5432_TCP_ADDR"),
+        "PORT": os.environ.get("DB_1_PORT_5432_TCP_PORT"),
     },
 }
 
 HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://{0}:{1}/'.format(
-            os.environ.get('ELASTICSEARCH_1_PORT_9200_TCP_ADDR'),
-            os.environ.get('ELASTICSEARCH_1_PORT_9200_TCP_PORT'),
+    "default": {
+        "ENGINE": "haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine",
+        "URL": "http://{0}:{1}/".format(
+            os.environ.get("ELASTICSEARCH_1_PORT_9200_TCP_ADDR"),
+            os.environ.get("ELASTICSEARCH_1_PORT_9200_TCP_PORT"),
         ),
-        'INDEX_NAME': 'docker',
+        "INDEX_NAME": "docker",
     },
 }
